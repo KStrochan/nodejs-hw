38126876dc86cb6a47adac0d8a2e66663296d58f
Konstantyn <72542265+KStrochan@users.noreply.github.com>
2026-09-02 23:05:50 +0300

    3 fix

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -42,7 +42,7 @@ export const updateNote = async (req, res, next) => {
     const { noteId } = req.params;
 
     const updatedNote = await Note.findByIdAndUpdate(noteId, req.body, {
-      new: true,
+      returnDocument: 'after',
     });
 
     if (!updatedNote) {
